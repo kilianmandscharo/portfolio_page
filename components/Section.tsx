193bae2fc832d02a6b300668286fd0503dabeaa2
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-16 17:35:48 +0100

    changed colors

--- components/Section.tsx
+++ components/Section.tsx
@@ -5,11 +5,11 @@ interface SectionProps {
 
 const Section = ({ children, name }: SectionProps) => {
     return (
-        <section id={name.toLowerCase()} className="text-white p-4 sm:p-8">
+        <section id={name.toLowerCase()} className="p-4 sm:p-8">
             <div className="flex justify-around items-center mb-4">
-                <div className="h-[2px] w-3/12 bg-white" />
+                <div className="h-[2px] w-3/12" />
                 <h2 className="text-4xl text-center mx-2 sm:mb-4">{name}</h2>
-                <div className="h-[2px] w-3/12 bg-white" />
+                <div className="h-[2px] w-3/12" />
             </div>
             <div className="text-[16px] sm:text-xl sm:columns-2 gap-x-8">
                 {children}
