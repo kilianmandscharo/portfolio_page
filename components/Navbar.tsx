06b70d31f45c901735983cbb9fe980db41692730
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-16 17:44:53 +0100

    changed colors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,10 @@ const Navbar = () => {
     return (
         <nav className="mb-8">
             <ul className="flex flex-row justify-around">
-                <li className="hover:text-black transition-all ease-linear">
+                <li className="hover:text-green-600 transition-all ease-linear">
                     <a href="#about">About</a>
                 </li>
-                <li className="hover:text-black transition-all ease-linear">
+                <li className="hover:text-green-600 transition-all ease-linear">
                     <a href="#projects">Projects</a>
                 </li>
             </ul>
--- components/Section.tsx
+++ components/Section.tsx
@@ -7,9 +7,9 @@ const Section = ({ children, name }: SectionProps) => {
     return (
         <section id={name.toLowerCase()} className="p-4 sm:p-8">
             <div className="flex justify-around items-center mb-4">
-                <div className="h-[2px] w-3/12" />
+                <div className="h-[2px] w-3/12 bg-black" />
                 <h2 className="text-4xl text-center mx-2 sm:mb-4">{name}</h2>
-                <div className="h-[2px] w-3/12" />
+                <div className="h-[2px] w-3/12 bg-black" />
             </div>
             <div className="text-[16px] sm:text-xl sm:columns-2 gap-x-8">
                 {children}
