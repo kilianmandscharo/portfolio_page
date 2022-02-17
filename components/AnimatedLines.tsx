08781d70dc247d8b310c80dd9e167027f686bd44
Dominik Heller <kilianmandscharo@gmail.com>
2022-02-17 19:37:12 +0100

    added animations with react spring

--- components/AnimatedLines.tsx
+++ components/AnimatedLines.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useSpring, animated } from "react-spring";
+
 interface AnimatedLinesProps {
     width: number;
     height: number;
@@ -11,13 +14,45 @@ const AnimatedLines = ({
     scrollY,
     projectsYPosition,
 }: AnimatedLinesProps) => {
-    const extraOffset =
-        scrollY > 117 ? (scrollY > projectsYPosition - 30 ? 60 : 30) : 0;
+    const [pathsAnimated, setPathsAnimated] = useState(false);
+
     const makeCircle = () => {
         return `a 5,5 0 0,0 0,20 a 5,5 0 0,0 0,-20 a 5,5 0 0,0 0,20`;
     };
+
+    const extraOffset =
+        scrollY > 117 ? (scrollY > projectsYPosition - 30 ? 60 : 30) : 0;
     const blueOffset = width / 1.05 - 31.4159 - 45;
     const purpleOffset = width / 1.12 - 41;
+
+    const { greenAnimatedOffset } = useSpring({
+        reset: true,
+        from: { greenAnimatedOffset: height },
+        to: { greenAnimatedOffset: height - 179 },
+        delay: 200,
+        config: {
+            duration: 1000,
+        },
+    });
+    const { purpleAnimatedOffset } = useSpring({
+        reset: true,
+        from: { purpleAnimatedOffset: height },
+        to: { purpleAnimatedOffset: height - purpleOffset - 70 },
+        delay: 200,
+        config: {
+            duration: 1000,
+        },
+    });
+    const { blueAnimatedOffset } = useSpring({
+        reset: true,
+        from: { blueAnimatedOffset: height },
+        to: { blueAnimatedOffset: height - blueOffset - 73 },
+        delay: 200,
+        config: {
+            duration: 1000,
+        },
+        onRest: () => setPathsAnimated(true),
+    });
 
     return (
         <svg
@@ -26,36 +61,45 @@ const AnimatedLines = ({
             xmlns="http://www.w3.org/2000/svg"
             className="absolute top-0 left-0"
         >
-            <path
-                d={`M 17 34 ${makeCircle()} v 140 ${makeCircle()} V ${projectsYPosition} ${makeCircle()} v 300`}
+            <animated.path
+                d={`M 17 34 ${makeCircle()} v 140 ${makeCircle()} V ${projectsYPosition} ${makeCircle()} V ${height}`}
                 stroke="limegreen"
                 strokeWidth="2"
                 fill="transparent"
                 strokeDasharray={height}
-                strokeDashoffset={height - 179 - scrollY - extraOffset}
+                strokeDashoffset={
+                    pathsAnimated
+                        ? height - 179 - scrollY - extraOffset
+                        : greenAnimatedOffset
+                }
             />
-            <path
+            <animated.path
                 d={`M ${
                     width / 1.12
-                } 80 H 41 a 20,20 0 0,0 -20,20 v 94 ${makeCircle()} V ${projectsYPosition} ${makeCircle()} v 300`}
+                } 80 H 41 a 20,20 0 0,0 -20,20 v 94 ${makeCircle()} V ${projectsYPosition} ${makeCircle()} V ${height}`}
                 stroke="#FC79FF"
                 strokeWidth="2"
                 fill="transparent"
                 strokeDasharray={height}
                 strokeDashoffset={
-                    height - scrollY - 70 - purpleOffset - extraOffset
+                    pathsAnimated
+                        ? height - scrollY - 70 - purpleOffset - extraOffset
+                        : purpleAnimatedOffset
                 }
             />
-            <path
+            <animated.path
                 d={`M ${
                     width / 1.05
-                } 100 v 30 a 20,20 0 0,1 -20,20 H 45 a 20,20 0 0,0 -20,20 v 24 ${makeCircle()} V ${projectsYPosition} ${makeCircle()} v 300`}
+                } 100 v 30 a 20,20 0 0,1 -20,20 H 45 a 20,20 0 0,0 -20,20 v 24 ${makeCircle()} V ${projectsYPosition} ${makeCircle()} V ${height}`}
                 stroke="#8FF8FF"
                 strokeWidth="2"
                 fill="transparent"
                 strokeDasharray={height}
                 strokeDashoffset={
-                    height - scrollY - 73 - blueOffset - extraOffset
+                    // height - scrollY - 73 - blueOffset - extraOffset
+                    pathsAnimated
+                        ? height - scrollY - 73 - blueOffset - extraOffset
+                        : blueAnimatedOffset
                 }
             />
         </svg>
